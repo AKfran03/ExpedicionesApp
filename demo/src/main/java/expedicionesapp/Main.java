53438cee2894eca8dbf787159983de32ebbcb836
Tomas Benjamin Ferreyra <tomas.benjamin.f@gmail.com>
2025-06-13 21:11:09 -0300

    Instrucciones BD
    Orden a seguir para la carga de datos en la base de datos.

--- demo/src/main/java/expedicionesapp/Main.java
+++ demo/src/main/java/expedicionesapp/Main.java
@@ -8,3 +8,32 @@ public class Main {
         c.mostrarMiembros();
     }
 }
+/*
+lo que necesitamos:
+	id expedicion, id fecha, id accidente, id miembro, id pico, id resultado, id ruta, altitud, cupos, conteo mortalidad.
+
+Como lo tenemos:
+	id_expedicion,codigo_pico,Año,host,Temporada,nombre_Temporada,Pais,Lideres,Exito,motivo_terminacion,altitud,cupo,Muertes,Accidente,Ruta,Fecha,Dia_Mas_Alto
+
+El orden a seguir para crear las bases de datos va a ser el siguente.
+
+1-[dbo].[Fecha]
+2-[dbo].[Accidente]
+3-[dbo].[Nacionalidad] (para miembros.)
+4-[dbo].[Miembros]
+5-[dbo].[Host]
+6-[dbo].[Localizacion]
+7-[dbo].[Pico]
+8-[dbo].[Motivo]
+9-[dbo].[Resultado]
+10-[dbo].[Ruta]
+11-[dbo].[Expediciones]
+
+
+1- Para fecha necesitamos:
+	fecha será sacado de la tabla expediciones.
+	fecha (date), año (int), temporada (int [verano = 2, otoño = 3, invierno = 4, primavera = 1, ya esta asi, no se pregunta.. xd]);
+	Fecha,Año,Temporada
+2- para Accidente
+
+*/
